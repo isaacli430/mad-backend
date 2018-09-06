82d584ae3f7b72cd7d7afc2e0367e2f4a825281d
kwugfighter <isaacli430@gmail.com>
2018-09-06 12:47:51 +0800

    a

---
+++ main.py
@@ -0,0 +1,17 @@
+from sanic import Sanic
+from sanic.response import html
+from sanic.blueprints import Blueprint
+import json
+
+class GlobalVars:
+    def __init__(self):
+        pass
+
+app = Sanic(__name__)
+
+gvars = GlobalVars()
+
+@app.route('/post', methods=["POST"])
+async def main(request):
+    data = request.json
+    return json({ "received": True, "message": request.json })
